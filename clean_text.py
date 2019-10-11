b42ae138c1e4bc8b9ecce8bd062500e6a3d8c5c0
Bo-Wei Tseng <32538529+R06942098@users.noreply.github.com>
2019-10-11 20:53:31 +0800

    Add files via upload

--- clean_text.py
+++ clean_text.py
@@ -11,6 +11,49 @@ from collections import defaultdict
 from keras.preprocessing.text import Tokenizer, text_to_word_sequence
 from keras.preprocessing.sequence import pad_sequences
 
+
+def parse_raw_message(raw_message):
+    lines = raw_message.split('\n')
+    email = {}
+    message = ''
+    keys_to_extract = ['from', 'to']
+    for line in lines:
+        if ':' not in line:
+            message += line.strip()
+            email['body'] = message
+        else:
+            pairs = line.split(':')
+            key = pairs[0].lower()
+            val = pairs[1].strip()
+            if key in keys_to_extract:
+                email[key] = val
+    return email
+
+def parse_into_emails(messages):
+    emails = [parse_raw_message(message) for message in messages]
+    return {
+        'body': map_to_list(emails, 'body'),
+        'to': map_to_list(emails, 'to'),
+        'from_': map_to_list(emails, 'from'),
+        'subject': list(email_old["Subject"])
+    }
+
+def map_to_list(emails, key):
+    results = []
+    for email in emails:
+        if key not in email:
+            results.append('')
+        else:
+            results.append(email[key])
+    return results
+
+#email_df = pd.read_csv("enron_all.csv",na_filter= False)
+#email = pd.read_csv("emails.csv")
+#email_df = pd.DataFrame(parse_into_emails(email.message))
+#print(email_df['subject'].values[12])
+#print(email_df['body'].values[517385])
+#email_df.to_csv("enron_min.csv", index=False)
+
 clean = True
 
 if clean:
@@ -34,7 +77,9 @@ if clean:
 		if k in ["", "\n\n"]:
 			list_content.append("Empty")
 		else:
-			list_content.append(k.split(" -----Original Message-----\n")[0])
+			#k = k.replace("\n", "")
+			#k = k.replace("\t", "")
+			list_content.append(k)
 
 	email_dict = {}
 	email_dict["subject"] = list_subject
